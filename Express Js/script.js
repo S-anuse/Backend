e2abd313691eaf81c69c82bc3472b354480d5f23
Sanjeevani <sanjeevanianuse@gmail.com>
2026-05-19 23:58:45 +0530

    learned about the form handling, cookies, blob

--- Express Js/script.js
+++ Express Js/script.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use((req,res,next)=>{
     console.log("middleware 1");
